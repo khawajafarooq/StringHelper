c0d6075263cf4c50002810e5bceadadc5b0c6fef
Khawaja Farooq <kfarooqa@gmail.com>
2017-10-10 20:01:23 +0300

    computed properties

--- StringPlayground.playground/Contents.swift
+++ StringPlayground.playground/Contents.swift
@@ -5,36 +5,35 @@ import Foundation
 extension String {
     
     /* Get length of string */
-    func length() -> Int {
+    var length: Int {
         return self.characters.count
     }
     
     /* Trim leading or trailing white spaces */
-    func trim() -> String {
-        return self.stringByTrimmingCharactersInSet(NSCharacterSet.whitespaceCharacterSet())
+    var trim: String {
+        return self.trimmingCharacters(in: .whitespaces)
     }
 
     /* Check if string contains a single numeric digit */
-    func isSingleNumericDigit() -> Bool {
-        if self.length() != 1 {
+    var isSingleNumericDigit: Bool {
+        if self.length != 1 {
             return false
         }
         else {
             let numberPredicate: NSPredicate = NSPredicate(format: "SELF MATCHES '^[0-9]+$'", 1)
-            return numberPredicate.evaluateWithObject(self)
+            return numberPredicate.evaluate(with: self)
         }
     }
 }
 
-
 var str = " Hello, playground "
 
-str.length()
-str.trim()
-str.isSingleNumericDigit()
+str.length
+str.trim
+str.isSingleNumericDigit
 
 str = "c"
-str.isSingleNumericDigit()
+str.isSingleNumericDigit
 
 str = "1"
-str.isSingleNumericDigit()
+str.isSingleNumericDigit
